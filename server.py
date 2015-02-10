44d4960849225d8735b989737c1aa0dba0062cce
root <root@Jacky>
2015-02-10 11:31:16 +0100

    Signed-off-by: root <root@Jacky>

---
+++ server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from PodSixNet.Channel import Channel
+from PodSixNet.Server import Server
+import time,sys, pygame
+from pygame.locals import *
+
+
+class ClientChannel(Channel):
+
+	def __init__(self, *args, **kwargs):
+		Channel.__init__(self, *args, **kwargs)
+
+	def Close(self):
+		self._server.del_client(self)
+
+	def Network(self,data):
+		print('message de type %s recu' % data['action'])
+
+
+
+class MyServer(Server):
+
+	channelClass = ClientChannel
+
+	def __init__(self, *args, **kwargs):
+		Server.__init__(self, *args, **kwargs)
+		self.clients = []
+		print('Server launched')
+
+	def Connected(self, channel, addr):
+		print('New connection')
+		self.clients.append(channel)
+
+	def del_client(self,channel):
+		print('client deconnected')
+		self.clients.remove(channel)
+
+def main_prog():
+	my_server = MyServer(localaddr = (sys.argv[1],int(sys.argv[2])))
+
+	while True:
+		my_server.Pump()
+		time.sleep(0.01)
+
+if __name__ == '__main__':
+	main_prog()
+	sys.exit(0)
